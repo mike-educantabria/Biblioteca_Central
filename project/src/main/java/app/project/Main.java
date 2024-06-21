317566cabcec25c981b4261a31f6678d05330d0b
mike-educantabria <mgutierrezc41@educantabria.es>
2024-06-21 18:46:36 +0200

    - Proyecto de NetBeans subido.

---
+++ project/src/main/java/app/project/Main.java
@@ -0,0 +1,13 @@
+/*
+ * @nombre: Miguel
+ * @apellidos: Gutiérrez Caso
+ */
+package app.project;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
